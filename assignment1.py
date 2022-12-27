b7c207faa0d0bbd7488e4aed012819b739a3a14d
MZRoscoe <103299768+MZRoscoe@users.noreply.github.com>
2022-12-27 21:11:41 +0700

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -3,4 +3,14 @@ from dataset import data
 filename = input()
 
 # ----- Write your code here -----
+def find_file(data, filename):
+    id = []
+    for i in data:
+        if i['is_file'] == True:
+            if i['name'] == filename:
+                id.append(i['id'])
+        else:
+            id += find_file(i['children'], filename)
+    return id
 
+print(find_file(data, filename))
